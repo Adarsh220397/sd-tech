4736b9472d58d5af13727fe698fb9066fe41fcd6
Adarsh <vadarsh22@gmail.com>
2023-03-06 15:34:25 +0530

    home screen made responsive

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,26 +5,27 @@ export default function Main() {
   return (
     <>
   
-      <div >
+   
+
       <div className={styles.bgImage}>
   </div>
           <div className={styles.homeHeaderContent}>
-            <div className={styles.container}>
-              <div className={styles.homeContentInner}>
+           <div className={styles.container}>
+             <div className={styles.homeContentInner}>
                 <h1>
                   <div> {"WE DON'T JUST DESIGN"}</div>
 
                   <div> {"WE DEFINE EXPERIENCES"}</div>
                 </h1>
-                <p className={styles.homeContentInnerP}>
+                <p >
                   {
                     "We are a bunch of smart and passionate people looking to reinvent the possibilities when technology and design meet."
                   }
                 </p>
-              </div>
+              </div> 
             </div>
           </div>
-        </div>
+     
    
 
     </>
